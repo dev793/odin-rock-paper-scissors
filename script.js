e50219bab2c8a2d18ce58a7bba1f3890f5d5ec85
dev793 <117709900+dev793@users.noreply.github.com>
2024-09-11 10:50:16 +1200

    Capture human choice and add score variables

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ function getComputerChoice() {
     let randomValue = Math.floor(Math.random() * 3); 
     switch (randomValue) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
+function getHumanChoice() {
+    let humanChoice = "";
+    while (humanChoice != "rock" && humanChoice != "paper" && humanChoice != 'scissors') {
+        humanChoice = prompt("Please choose 'rock', 'paper', or 'scissors':");
+    }
+    return humanChoice;
+}
+
+let humanScore = 0;
+let computerScore = 0;
