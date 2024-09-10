a5f30b4ef2fa1d4fde128d7e99df1dbb9fd2aa05
dev793 <117709900+dev793@users.noreply.github.com>
2024-09-11 10:26:04 +1200

    Create computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+    let randomValue = Math.floor(Math.random() * 3); 
+    switch (randomValue) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
+
