9a1a9177c36256f50c35be81c4c9a6ae7958c6cd
dev793 <117709900+dev793@users.noreply.github.com>
2025-12-01 11:44:37 +1300

    Add get human choice logic

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
+
+function getHumanChoice() {
+    return humanChoice = prompt("Make your choice (rock, paper, scissors):");
+    
+}
