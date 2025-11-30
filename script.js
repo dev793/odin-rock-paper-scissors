72904228e8c49405827bd98a7d1d00ac4e786af6
dev793 <117709900+dev793@users.noreply.github.com>
2025-12-01 12:27:41 +1300

    Add logic to play one round

--- script.js
+++ script.js
@@ -7,6 +7,38 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return humanChoice = prompt("Make your choice (rock, paper, scissors):");
-    
+    return humanChoice = prompt("Make your choice (rock, paper, scissors):").toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    let result = "";
+    
+    if (humanChoice == computerChoice) {
+        result = "draw";
+    } else if (humanChoice == "rock") {
+        result = (computerChoice == "scissors") ? "win" : "lose";
+    } else if (humanChoice == "paper") {
+        result = (computerChoice == "rock") ? "win" : "lose";
+    } else if (humanChoice == "scissors") {
+        result = (computerChoice == "paper") ? "win" : "lose";
+    }
+
+    if (result == "win") {
+        humanScore += 1;
+        console.log(`You ${result}, ${humanChoice} beats ${computerChoice}`);
+    } else if (result == "lose") {
+        computerScore += 1;
+        console.log(`You ${result}, ${computerChoice} beats ${humanChoice}`);
+    } else {
+        console.log(`You ${result}, both players chose ${humanChoice}`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Player choice: " + humanSelection);
+console.log("Computer choice: " + computerSelection);
+
+playRound(humanSelection, computerSelection);
