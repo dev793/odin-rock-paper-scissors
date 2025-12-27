6e09000814e5b43445d6c2b283f7b4c3d0b68070
dev793 <117709900+dev793@users.noreply.github.com>
2025-12-27 22:21:53 +1300

    minor tidy up of redundant code (overall still a mess)

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 let humanScore = 0;
 let computerScore = 0;
-let choices = ["rock", "paper", "scissors"];
+const choices = ["rock", "paper", "scissors"];
 const results = document.querySelector("#results")
 const outcome = document.querySelector("#outcome");
 const score = document.querySelector("#score");
+const buttonnContainer = document.querySelector("#buttons");
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
-}
-
-function getHumanChoice() {
-    return humanChoice = prompt("Make your choice (rock, paper, scissors):").toLowerCase();
 }
 
 function playRound(humanChoice, computerChoice) {
@@ -61,8 +58,6 @@ function buttonPress(event) {
     playRound(humanChoice, getComputerChoice());
 }
 
-const buttonnContainer = document.querySelector("#buttons");
-
 choices.forEach((choice) => {
     const button = document.createElement("button");
     button.textContent = choice;
@@ -70,24 +65,3 @@ choices.forEach((choice) => {
 
     button.addEventListener('click', buttonPress);
 })
-
-
-//playRound(getHumanChoice(), getComputerChoice());
-
-// function playGame() {
-
-//     for (let i = 0; i < 5; i++ ) {
-//         playRound(getHumanChoice(), getComputerChoice());
-//         console.log(`Player Score: ${humanScore}, Computer score: ${computerScore}`);
-//     }
-
-//     if (humanScore > computerScore) {
-//         console.log(`You win the game! Player Score: ${humanScore}, Computer score: ${computerScore}`);
-//     } else if (computerScore > humanScore) {
-//         console.log(`You lose the game! Player Score: ${humanScore}, Computer score: ${computerScore}`);
-//     } else {
-//         console.log(`It's a tie! Player Score: ${humanScore}, Computer score: ${computerScore}`);
-//     }
-// }
-
-// playGame();
